a86ee66ffe591a2892c280cafb1a71d7f4dde237
Bijan Haney <bijan@viam.com>
2026-03-26 13:22:23 -0400

    first commit

---
+++ sam2-detector/README.md
@@ -0,0 +1,9 @@
+# Module sam2-detector 
+
+Provide a description of the purpose of the module and any relevant information.
+
+## Models
+
+This module provides the following model(s):
+
+- [`viam:sam2-detector:sam2`](viam_sam2-detector_sam2.md) - Provide a brief description of the model
---
+++ sam2-detector/src/models/sam2.py
@@ -0,0 +1,139 @@
+import sys
+from typing import ClassVar, Final, List, Mapping, Optional, Sequence, Tuple
+
+from typing_extensions import Self
+from viam.media.video import ViamImage
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import PointCloudObject, ResourceName
+from viam.proto.service.vision import (Classification, Detection,
+                                       GetPropertiesResponse)
+from viam.resource.base import ResourceBase
+from viam.resource.easy_resource import EasyResource
+from viam.resource.types import Model, ModelFamily
+from viam.services.vision import *
+from viam.utils import ValueTypes
+
+
+class Sam2(Vision, EasyResource):
+    # To enable debug-level logging, either run viam-server with the --debug option,
+    # or configure your resource/machine to display debug logs.
+    MODEL: ClassVar[Model] = Model(ModelFamily("viam", "sam2-detector"), "sam2")
+
+    @classmethod
+    def new(
+        cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ) -> Self:
+        """This method creates a new instance of this Vision service.
+        The default implementation sets the name from the `config` parameter.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+            dependencies (Mapping[ResourceName, ResourceBase]): The dependencies (both required and optional)
+
+        Returns:
+            Self: The resource
+        """
+        return super().new(config, dependencies)
+
+    @classmethod
+    def validate_config(
+        cls, config: ComponentConfig
+    ) -> Tuple[Sequence[str], Sequence[str]]:
+        """This method allows you to validate the configuration object received from the machine,
+        as well as to return any required dependencies or optional dependencies based on that `config`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+
+        Returns:
+            Tuple[Sequence[str], Sequence[str]]: A tuple where the
+                first element is a list of required dependencies and the
+                second element is a list of optional dependencies
+        """
+        return [], []
+
+    async def capture_all_from_camera(
+        self,
+        camera_name: str,
+        return_image: bool = False,
+        return_classifications: bool = False,
+        return_detections: bool = False,
+        return_object_point_clouds: bool = False,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> CaptureAllResult:
+        self.logger.error("`capture_all_from_camera` is not implemented")
+        raise NotImplementedError()
+
+    async def get_detections_from_camera(
+        self,
+        camera_name: str,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Detection]:
+        self.logger.error("`get_detections_from_camera` is not implemented")
+        raise NotImplementedError()
+
+    async def get_detections(
+        self,
+        image: ViamImage,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Detection]:
+        self.logger.error("`get_detections` is not implemented")
+        raise NotImplementedError()
+
+    async def get_classifications_from_camera(
+        self,
+        camera_name: str,
+        count: int,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Classification]:
+        self.logger.error("`get_classifications_from_camera` is not implemented")
+        raise NotImplementedError()
+
+    async def get_classifications(
+        self,
+        image: ViamImage,
+        count: int,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[Classification]:
+        self.logger.error("`get_classifications` is not implemented")
+        raise NotImplementedError()
+
+    async def get_object_point_clouds(
+        self,
+        camera_name: str,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> List[PointCloudObject]:
+        self.logger.error("`get_object_point_clouds` is not implemented")
+        raise NotImplementedError()
+
+    async def get_properties(
+        self,
+        *,
+        extra: Optional[Mapping[str, ValueTypes]] = None,
+        timeout: Optional[float] = None,
+    ) -> Vision.Properties:
+        self.logger.error("`get_properties` is not implemented")
+        raise NotImplementedError()
+
+    async def do_command(
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs,
+    ) -> Mapping[str, ValueTypes]:
+        self.logger.error("`do_command` is not implemented")
+        raise NotImplementedError()
+
